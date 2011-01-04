1b863a2446858245d22d34a37b3df24958c7333b
Felix H. Dahlke <fhd@ubercode.de>
2011-01-04 01:30:56 +0100

    Did a quick hack to see whether this will work.

--- com.github.fhd.eclipsecolortheme/src/com/github/fhd/eclipsecolortheme/preferences/ColorThemePreferencePage.java
+++ com.github.fhd.eclipsecolortheme/src/com/github/fhd/eclipsecolortheme/preferences/ColorThemePreferencePage.java
@@ -1,7 +1,9 @@
 package com.github.fhd.eclipsecolortheme.preferences;
 
+import org.eclipse.core.runtime.preferences.*;
 import org.eclipse.jface.preference.*;
 import org.eclipse.ui.*;
+import org.osgi.service.prefs.*;
 
 import com.github.fhd.eclipsecolortheme.*;
 
@@ -31,6 +33,29 @@ public class ColorThemePreferencePage extends FieldEditorPreferencePage
 	@Override
 	public boolean performOk() {
 	    colorThemeEditor.store();
+	    IEclipsePreferences prefs = new InstanceScope().getNode(
+               "org.eclipse.ui.editors");
+	    if ("dark".equals(getPreferenceStore().getString("colorTheme"))) {
+	        prefs.putBoolean(
+                    "AbstractTextEditor.Color.Foreground.SystemDefault",
+                    false);
+	        prefs.put("AbstractTextEditor.Color.Foreground", "255,255,255");
+	        prefs.putBoolean(
+	                "AbstractTextEditor.Color.Background.SystemDefault",
+                    false);
+	        prefs.put("AbstractTextEditor.Color.Background", "0,0,0");
+	    } else {
+	        prefs.remove("AbstractTextEditor.Color.Foreground.SystemDefault");
+	        prefs.remove("AbstractTextEditor.Color.Foreground");
+	        prefs.remove("AbstractTextEditor.Color.Background.SystemDefault");
+	        prefs.remove("AbstractTextEditor.Color.Background");
+	    }
+	    try {
+            prefs.flush();
+        } catch (BackingStoreException e) {
+            // TODO: Show a proper error message (StatusManager).
+            e.printStackTrace();
+        }
 	    return super.performOk();
 	}
 }
