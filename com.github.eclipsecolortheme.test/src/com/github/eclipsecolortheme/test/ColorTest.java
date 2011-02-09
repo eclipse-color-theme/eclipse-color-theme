e6bbe0f425f2bdc1ff07854013dc0c44ca9dd4cb
Roger Dudler <roger.dudler@gmail.com>
2011-02-09 10:34:53 +0100

    Added test project.

---
+++ com.github.eclipsecolortheme.test/src/com/github/eclipsecolortheme/test/ColorTest.java
@@ -0,0 +1,21 @@
+package com.github.eclipsecolortheme.test;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+import com.github.eclipsecolortheme.Color;
+
+public class ColorTest {
+
+	@Test
+	public void testColor() {
+		Color color = new Color("#FFAA00");
+		Assert.assertEquals("255,170,0", color.asRGB());
+		Assert.assertEquals("#FFAA00", color.asHex().toUpperCase());
+		Assert.assertEquals(255, color.getR().intValue());
+		Assert.assertEquals(170, color.getG().intValue());
+		Assert.assertEquals(0, color.getB().intValue());
+	}
+
+}
