8ba94af192cf80d0b99b2d7292b86aca6f6ba26c
Felix H. Dahlke <fhd@ubercode.de>
2011-02-15 07:34:11 +0100

    Fixed the JavaScript mapping.

--- com.github.eclipsecolortheme/src/com/github/eclipsecolortheme/mapper/WebEditorMapper.java
+++ com.github.eclipsecolortheme/src/com/github/eclipsecolortheme/mapper/WebEditorMapper.java
@@ -4,8 +4,6 @@ import org.eclipse.core.runtime.preferences.IEclipsePreferences;
 
 import com.github.eclipsecolortheme.ColorThemeMapping;
 import com.github.eclipsecolortheme.ColorThemeSetting;
-import com.github.eclipsecolortheme.ColorThemeSemanticHighlightingMapping;
-
 
 /**
  * Maps color themes to preferences for Eclipse's XML, HTML and CSS editors.
@@ -15,12 +13,6 @@ public class WebEditorMapper extends GenericMapper {
     @Override
     protected ColorThemeMapping createMapping(String pluginKey, String themeKey) {
     	return new Mapping(pluginKey, themeKey);
-    }
-    
-    @Override
-    protected ColorThemeSemanticHighlightingMapping createSemanticHighlightingMapping(
-    		String pluginKey, String themeKey) {
-    	return new SemanticMapping(pluginKey, themeKey);
     }
     
     private class Mapping extends ColorThemeMapping {
@@ -40,29 +32,6 @@ public class WebEditorMapper extends GenericMapper {
 			preferences.put(pluginKey, value);
 		}
 
-	}
-	
-	private class SemanticMapping extends ColorThemeSemanticHighlightingMapping {
-		
-		public SemanticMapping(String pluginKey, String themeKey) {
-			super(pluginKey, themeKey);
-		}
-
-		@Override
-		public void putPreferences(IEclipsePreferences preferences,
-				ColorThemeSetting setting) {
-			preferences.putBoolean(pluginKey + ".enabled", true);
-			preferences.put(pluginKey + ".color", setting.getColor().asHex());
-			if (setting.isBoldEnabled() != null)
-				preferences.putBoolean(pluginKey + ".bold", setting.isBoldEnabled());
-			if (setting.isItalicEnabled() != null)
-				preferences.putBoolean(pluginKey + ".italic", setting.isItalicEnabled());
-			if (setting.isUnderlineEnabled() != null)
-				preferences.putBoolean(pluginKey + ".underline", setting.isUnderlineEnabled());
-			if (setting.isStrikethroughEnabled() != null)
-				preferences.putBoolean(pluginKey + ".strikethrough", setting.isStrikethroughEnabled());
-		}
-		
 	}
     
 }
