1453d2bbfb3fcf3cc15a59fa01f93c9dc35b2738
Péter Szilágyi <peter@karalabe.com>
2011-08-05 11:51:26 +0300

    Added support for italic and bold fonts for PyDev.
    Implemented a custom python mapper to enable saving font styles too
    besides the colors.

---
+++ com.github.eclipsecolortheme/src/com/github/eclipsecolortheme/mapper/PythonEditorMapper.java
@@ -0,0 +1,34 @@
+package com.github.eclipsecolortheme.mapper;
+
+import org.eclipse.core.runtime.preferences.IEclipsePreferences;
+
+import com.github.eclipsecolortheme.ColorThemeMapping;
+import com.github.eclipsecolortheme.ColorThemeSetting;
+
+public class PythonEditorMapper extends GenericMapper {
+	private class Mapping extends ColorThemeMapping {
+		public Mapping(String pluginKey, String themeKey) {
+			super(pluginKey, themeKey);
+		}
+
+		@Override
+		public void putPreferences(IEclipsePreferences preferences, ColorThemeSetting setting) {
+			preferences.put(pluginKey, setting.getColor().asRGB());
+			
+			String styleKey = pluginKey.replaceAll("COLOR", "STYLE");
+			int styleVal = 0;
+			if (setting.isBoldEnabled() != null && setting.isBoldEnabled()) {
+				styleVal += 1;
+			}
+			if (setting.isItalicEnabled() != null && setting.isItalicEnabled()) {
+				styleVal += 2;
+			}
+			preferences.putInt(styleKey, styleVal);
+		}
+	}
+	
+	@Override
+    protected ColorThemeMapping createMapping(String pluginKey, String themeKey) {
+    	return new Mapping(pluginKey, themeKey);
+    }
+}
