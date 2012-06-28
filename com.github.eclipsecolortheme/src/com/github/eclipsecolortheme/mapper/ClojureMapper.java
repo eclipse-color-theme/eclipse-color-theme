8a74570cb4de3eab9648717fb38274c061c134dc
calebmpeterson <caleb.m.peterson@gmail.com>
2012-06-28 00:42:14 -0500

    added support for Clojure/CCW

---
+++ com.github.eclipsecolortheme/src/com/github/eclipsecolortheme/mapper/ClojureMapper.java
@@ -0,0 +1,40 @@
+package com.github.eclipsecolortheme.mapper;
+
+import org.eclipse.core.runtime.preferences.IEclipsePreferences;
+
+import com.github.eclipsecolortheme.ColorThemeMapping;
+import com.github.eclipsecolortheme.ColorThemeSetting;
+
+/**
+ * Eclipse Color Theme Mapper for Clojure/CCW
+ * @author Caleb Peterson (caleb.m.peterson@gmail.com)
+ */
+public class ClojureMapper extends GenericMapper {
+
+	private class Mapping extends ColorThemeMapping {
+
+		public Mapping(String pluginKey, String themeKey) {
+			super(pluginKey, themeKey);
+		}
+		
+		@Override
+		public void putPreferences(IEclipsePreferences preferences,
+				ColorThemeSetting setting) {
+			// Set the color itself
+			super.putPreferences(preferences, setting);
+			// Enable styling
+			preferences.putBoolean(pluginKey + ".enabled", true);
+			// Bold?
+			preferences.putBoolean(pluginKey + ".bold", setting.isBoldEnabled() != null && setting.isBoldEnabled());
+			// Italics?
+			preferences.putBoolean(pluginKey + ".italic", setting.isItalicEnabled() != null && setting.isItalicEnabled());
+		}
+		
+	}
+	
+	@Override
+	protected ColorThemeMapping createMapping(String pluginKey, String themeKey) {
+		return new Mapping(pluginKey, themeKey);
+	}
+	
+}
